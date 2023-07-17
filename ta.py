4b620bfc8990047ab1b5c4f9bd3449d1967d0a34
12S19007_Delpia Yesica Marpaung <78125833+delpiamarpaung@users.noreply.github.com>
2023-07-17 10:35:36 +0700

    Update ta.py

--- ta.py
+++ ta.py
@@ -1,1 +1,14 @@
+#melakukan import library
 
+import warnings
+warnings.simplefilter("ignore")
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from IPython.display import Image
+import seaborn as sns
+import sys
+from sklearn.feature_selection import SelectKBest, chi2
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, classification_report
